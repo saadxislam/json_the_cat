24e34ab93c97ed42f13e15b34bdd9b8ab7ef6766
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-09-24 22:27:54 +0000

    before m&c setup. everything working good now

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,25 @@
 const request = require('request');
 
-const arg = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    } else {
+      // console.log(body)                //notice that this is a string
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback("Breed doesn't exist", null);
+        return;
+      }
+      callback(null, data[0].description);
+    }
+  });
+};
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + arg[0]
-
-request(url, (error, response, body) => {
-  if (error === undefined){
-    console.log('NOPE!');
-    process.exit;
-  } else {
-    // console.log(body)                //notice that this is a string
-    const data = JSON.parse(body);       // serialize it into an object
-    // console.log(data);
-    // console.log(typeof data);
-    console.log(data[0].description);
-  }
-});
 
 
+
+module.exports = { fetchBreedDescription };
 
